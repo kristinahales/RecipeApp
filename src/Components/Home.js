63b63d0465e28373347f200afcc231124148abd2
kristinahales <kristinakfriel@gmail.com>
2022-10-04 18:42:44 -0600

    Changed title of app

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -5,7 +5,7 @@ class Home extends React.Component {
     render() {
       return   <div>
       <div className="main-header">My Recipes</div>
-      <h2 className='home-header'>Hi my name is Kristina Hales, Software Engineer. I'm a wife. mom, and foodie enthusiast. <br/> Click the sidebar to get a list of recipe ideas!</h2>
+      <h2 className='home-header'>Hi my name is Kristina Hales, Software Engineer. I'm a wife, mom, and foodie enthusiast. <br/> Click the sidebar to get a list of recipe ideas!</h2>
     </div>
     }
   }
